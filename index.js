5dcb9760b484b96df7d30b06beae952033b6ffb1
jaicedev <jaicedev@gmail.com>
2020-08-09 11:35:35 -0400

    Added Default Input and Minor Functionality Tweaks

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ function getNumberOfDogImages(){
     $('#js-dog-form').submit(function (e){
         $('#js-dog-container').html('');
         e.preventDefault();
-        let checkNum = $('#js-dog-num').val();
+        let checkNum = parseInt($('#js-dog-num').val());
+        if(checkNum >= 1 && checkNum <= 50){
+            checkNum = checkNum;
+        }
+        else{
+            checkNum = 3;
+            console.log('Invalid Input Given - Fetching Default')
+        }
         fetch(`https://dog.ceo/api/breeds/image/random/${checkNum}`)
             .then(response => response.json())
             .then(responseJson =>
@@ -22,27 +29,36 @@ function getBreedOfDog(){
             .then(response => response.json())
             .then(responseJson => {
                 if(responseJson.status === 'error'){
-                    console.log('Caught')
+                    alert('Invalid Input - Try Again Please')
+                    console.log('Invalid Input - Try Again Please')
+                    $('#js-dog-breed').val('')
                 }else{
                     showDogBreedImage(responseJson)
                 }
             })
-            .catch(error => alert(error))
+            .catch(error => alert('Error: ' + error))
     })
 }
 
 function showDogBreedImage(responseJson){
-        console.log(responseJson.message)
-        $('#js-dog-container').append(`<img src="${responseJson.message}" class="dog-image">`)
+    $('#js-dog-container').show();
+    console.log(responseJson)
+    $('#js-dog-container').append(`<img src="${responseJson.message}" class="dog-image">`)
+    $('#js-dog-breed').val('')
 }
 
 function showDogs(responseJson){
+    $('#js-dog-container').show();
     let numberOfDogs = responseJson.message.length;
     console.log(responseJson)
     for(let i = 0; i < numberOfDogs; i++){
         $('#js-dog-container').append(`<img src="${responseJson.message[i]}" class="dog-image">`)
     }
 }
+
+$(function(){
+    $('#js-dog-container').hide();
+})
 
 $(getBreedOfDog);
 $(getNumberOfDogImages);
